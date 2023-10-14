4a0d4f21161cbd0e60eddf6c4d2ae441b1c44b4c
JeonYera <vldzmf0877@naver.com>
2023-10-14 22:48:46 +0900

    db 연결 수정

--- study/src/test/java/com/rest/study/StudyApplicationTests.java
+++ study/src/test/java/com/rest/study/StudyApplicationTests.java
@@ -1,7 +1,5 @@
 package com.rest.study;
 
-import com.rest.study.board.foodboard.entity.FoodBoard;
-import com.rest.study.board.foodboard.repository.FoodBoardRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
